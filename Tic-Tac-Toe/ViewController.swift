bfaf70f1b61a895007a3d1377fb0df49dcc98e05
quichey <kylengo357@gmail.com>
2021-08-13 02:01:56 -0700

    drew tic-tac-toe grid

--- Tic-Tac-Toe/ViewController.swift
+++ Tic-Tac-Toe/ViewController.swift
@@ -9,10 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var squareLabel0: UILabel!
+    @IBOutlet var squareLabel1: UILabel!
+    @IBOutlet var squareLabel2: UILabel!
+    @IBOutlet var squareLabel3: UILabel!
+    @IBOutlet var squareLabel4: UILabel!
+    @IBOutlet var squareLabel5: UILabel!
+    @IBOutlet var squareLabel6: UILabel!
+    @IBOutlet var squareLabel7: UILabel!
+    @IBOutlet var squareLabel8: UILabel!
+    private var squareLabels: [[UILabel]] = [[], [], []]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        squareLabels = [[squareLabel0], [squareLabel1], [squareLabel2],
+                        [squareLabel3], [squareLabel4], [squareLabel5],
+                        [squareLabel6], [squareLabel7], [squareLabel8]]
+        for labelArr in squareLabels {
+            for label in labelArr {
+                label.text = ""
+            }
+        }
     }
 
 
