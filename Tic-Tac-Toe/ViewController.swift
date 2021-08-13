c71484bdda2715c2cc7742a13f001d16d096aa2b
quichey <kylengo357@gmail.com>
2021-08-12 21:27:25 -0700

    start xcode project

---
+++ Tic-Tac-Toe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tic-Tac-Toe
+//
+//  Created by Kyle Ngo on 8/12/21.
+//  Copyright © 2021 Kyle Ngo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
