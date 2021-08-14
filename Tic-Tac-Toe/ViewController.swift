ad05f311e8bebfa8495ec9100a3d81ae1b6d797b
quichey <kylengo357@gmail.com>
2021-08-14 02:47:28 -0700

    connected buttons to IBOutlets

--- Tic-Tac-Toe/ViewController.swift
+++ Tic-Tac-Toe/ViewController.swift
@@ -11,27 +11,27 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet var square0: UIButton!
     @IBOutlet var square1: UIButton!
-    @IBOutlet var squareButton2: UIButton!
-    @IBOutlet var squareButton3: UIButton!
-    @IBOutlet var squareButton4: UIButton!
-    @IBOutlet var squareButton5: UIButton!
-    @IBOutlet var squareButton6: UIButton!
-    @IBOutlet var squareButton7: UIButton!
-    @IBOutlet var squareButton8: UIButton!
+    @IBOutlet var square2: UIButton!
+    @IBOutlet var square3: UIButton!
+    @IBOutlet var square4: UIButton!
+    @IBOutlet var square5: UIButton!
+    @IBOutlet var square6: UIButton!
+    @IBOutlet var square7: UIButton!
+    @IBOutlet var square8: UIButton!
  
-    private var squareButtons: [[UIButton]] = [[], [], []]
+    private var squares: [[UIButton]] = [[], [], []]
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         /*
-        squareButtons = [[squareButton0], [squareButton1], [squareButton2],
-                        [squareButton3], [squareButton4], [squareButton5],
-                        [squareButton6], [squareButton7], [squareButton8]]*/
+        squares = [[square0], [square1], [square2],
+                        [square3], [square4], [square5],
+                        [square6], [square7], [square8]]*/
         
         /*
-        for ButtonArr in squareButtons {
+        for ButtonArr in squares {
             for Button in ButtonArr {
                 //Button.text = ""
             }
