33500f330f3dbcacfd60b11f26402c95ca92e596
quichey <kylengo357@gmail.com>
2021-08-14 04:05:52 -0700

    add buttonTouch wrapper for alternating X's and O's and buttonTouch IBAction for all 9 buttons

--- Tic-Tac-Toe/ViewController.swift
+++ Tic-Tac-Toe/ViewController.swift
@@ -21,24 +21,70 @@ class ViewController: UIViewController {
  
     private var squares: [[UIButton]] = [[], [], []]
     
+    private var numClicks: Int = 0
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        /*
+        
         squares = [[square0], [square1], [square2],
                         [square3], [square4], [square5],
-                        [square6], [square7], [square8]]*/
+                        [square6], [square7], [square8]]
         
-        /*
+        
         for ButtonArr in squares {
             for Button in ButtonArr {
-                //Button.text = ""
+                Button.setTitle("", for: .normal)
             }
         }
- */
+ 
+    }
+    
+    func buttonTouchWrapper(sender: UIButton) {
+        numClicks += 1
+        if (numClicks % 2 == 0) {
+            sender.setTitle("X", for: .normal)
+        }
+        else {
+            sender.setTitle("O", for: .normal)
+        }
     }
 
-
+    @IBAction func buttonTouch(sender: UIButton) {
+        buttonTouchWrapper(sender: sender)
+    }
+    
+    @IBAction func buttonTouch1(sender: UIButton) {
+        buttonTouchWrapper(sender: sender)
+    }
+    
+    @IBAction func buttonTouch2(sender: UIButton) {
+        buttonTouchWrapper(sender: sender)
+    }
+    
+    @IBAction func buttonTouch3(sender: UIButton) {
+        buttonTouchWrapper(sender: sender)
+    }
+    
+    @IBAction func buttonTouch4(sender: UIButton) {
+        buttonTouchWrapper(sender: sender)
+    }
+    
+    @IBAction func buttonTouch5(sender: UIButton) {
+        buttonTouchWrapper(sender: sender)
+    }
+    
+    @IBAction func buttonTouch6(sender: UIButton) {
+        buttonTouchWrapper(sender: sender)
+    }
+    
+    @IBAction func buttonTouch7(sender: UIButton) {
+        buttonTouchWrapper(sender: sender)
+    }
+    
+    @IBAction func buttonTouch8(sender: UIButton) {
+        buttonTouchWrapper(sender: sender)
+    }
 }
 
